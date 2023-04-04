6d174e1ed7629fa4ae78da7befd134ed495d32d8
Gian de Oliveira <naigdeoliveira@gmail.com>
2023-04-04 10:20:30 -0300

    menu-mobile

---
+++ sacole/src/assets/Home.jsx
@@ -0,0 +1,10 @@
+import "../App.css"
+import Navbar from "./Navbar.jsx"
+
+export default function Home(){
+    return(
+        <div className="image">
+            <Navbar/>
+        </div>
+    )
+}
---
+++ sacole/src/assets/Navbar.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+
+export default function Navbar() {
+
+    const [burger_class, setBurgerClass] = useState("burger-bar unclicked")
+    const [menu_class, setMenuClass] = useState("menu hidden")
+    const [isMenuClicked, setIsMenuClicked] = useState(false)
+
+    const updateMenu=()=> {
+        if(!isMenuClicked) {
+            setBurgerClass("burger-bar clicked")
+            setMenuClass("menu visible")
+        }
+        else {
+            setBurgerClass("burger-bar unclicked")
+            setMenuClass("menu hidden")
+        }
+        setIsMenuClicked(!isMenuClicked)
+    }
+    return (
+        <div className="w-full h-screen">
+            <nav>
+                <div className="burger-menu" onClick={updateMenu}>
+                    <div className={burger_class}></div>
+                    <div className={burger_class}></div>
+                    <div className={burger_class}></div>
+                </div>
+            </nav>
+
+            <div className={menu_class}>
+                
+            </div>
+        </div>
+    )
+}
