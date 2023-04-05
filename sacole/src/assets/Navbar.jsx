ac5f5e0268c3ee15a8de6401acbc7d8cea009c3f
Gian de Oliveira <naigdeoliveira@gmail.com>
2023-04-05 19:24:48 -0300

    fonts, button, nav

--- sacole/src/assets/Navbar.jsx
+++ sacole/src/assets/Navbar.jsx
@@ -6,8 +6,8 @@ export default function Navbar() {
     const [menu_class, setMenuClass] = useState("menu hidden")
     const [isMenuClicked, setIsMenuClicked] = useState(false)
 
-    const updateMenu=()=> {
-        if(!isMenuClicked) {
+    const updateMenu = () => {
+        if (!isMenuClicked) {
             setBurgerClass("burger-bar clicked")
             setMenuClass("menu visible")
         }
@@ -18,17 +18,25 @@ export default function Navbar() {
         setIsMenuClicked(!isMenuClicked)
     }
     return (
-        <div className="w-full h-screen">
+        <div className="w-full fixed top-0 left-0 z-10">
             <nav>
                 <div className="burger-menu" onClick={updateMenu}>
                     <div className={burger_class}></div>
                     <div className={burger_class}></div>
                     <div className={burger_class}></div>
                 </div>
+
+                <h1 className="text-4xl font-['Alkatra'] left-14 top-2 relative">Sacolé</h1>
+
             </nav>
 
             <div className={menu_class}>
-                
+                <div className="z-10 relative top-48 text-3xl list-none flex flex-col justify-center items-center font-[Dosis]">
+                    <li className="py-4"><a>Inicio</a></li>
+                    <li className="py-4"><a>Sabores</a></li>
+                    <li className="py-4"><a>Preços</a></li>
+                    <li className="py-4"><a>Contatos</a></li>
+                </div>
             </div>
         </div>
     )
