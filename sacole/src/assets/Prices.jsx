1a7e18b7ffd8861a251ce0eb924a9bceb28860de
Gian de Oliveira <naigdeoliveira@gmail.com>
2023-05-11 16:06:08 -0300

    logica do preço

--- sacole/src/assets/Prices.jsx
+++ sacole/src/assets/Prices.jsx
@@ -6,10 +6,10 @@ export default function Prices() {
         <section className="flex justify-center items-center flex-col h-screen relative md:h-0 md:top-52">
             <h2 className="text-3xl font-['Alkatra'] text-center text-[#efefef]">Refresque-se com nossos sacolés a preços acessíveis!</h2>
             <div className="md:flex">
-                <div className="border-orange-400 w-80 h-56 text-center bg-gradient-to-b from-orange-300 to-yellow-300 md:m-2">
+                <div className="border-orange-400 w-80 h-64 text-center bg-gradient-to-b from-orange-300 to-yellow-300 md:m-2">
                     <CommonItem />
                 </div>
-                <div className="border-orange-400 w-80 h-56 text-center bg-gradient-to-b from-orange-300 to-yellow-300 md:m-2">
+                <div className="border-orange-400 w-80 h-64 text-center bg-gradient-to-b from-orange-300 to-yellow-300 md:m-2">
                     <GourmetItem />
                 </div>
             </div>
--- sacole/src/assets/algorithm/GourmetItem.jsx
+++ sacole/src/assets/algorithm/GourmetItem.jsx
@@ -1,36 +1,44 @@
 import React, { useState } from "react";
 
 export default function GourmetItem(props) {
-    const [total, setTotal] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [exceededLimit, setExceededLimit] = useState(false);
 
-    function handleQuantityChange(event) {
-        const newQuantity = event.target.value;
-        const newTotal = newQuantity * 3;
-        setTotal(newTotal);
+  function handleQuantityChange(event) {
+    let newQuantity = event.target.value;
+
+    if (newQuantity > 100) {
+      newQuantity = 100;
+      setExceededLimit(true);
+    } else {
+      setExceededLimit(false);
     }
 
-    function handleKeyDown(e) {
-        e.preventDefault();
-      }
+    const newTotal = newQuantity * 3;
+    setTotal(newTotal);
+  }
 
-    return (
-        <div>
-            <div className="relative top-10">
-                <h2 className="text-xl">Sacolé Gourmet</h2>
-                <p className="text-6xl">R${total},00</p>
-            </div>
-            <label className="relative top-20 text-2xl">
-                Quantidade:
-                <input
-                    className="w-14 text-center caret-transparent outline-none"
-                    type="number"
-                    min="0"
-                    max="100"
-                    value={props.quantity}
-                    onChange={handleQuantityChange}
-                    onKeyDown={handleKeyDown}
-                />
-            </label>
-        </div>
-    );
+  return (
+    <div>
+      <div className="relative top-10">
+        <h2 className="text-xl">Sacolé Gourmet</h2>
+        <p className="text-6xl">R${total},00</p>
+      </div>
+      <label className="relative top-28 text-2xl">
+        Quantidade:
+        <input
+          className="w-14 text-center outline-none"
+          type="number"
+          min="0"
+          max="100"
+          value={props.quantity}
+          onChange={handleQuantityChange}
+        />
+      </label>
+
+      {exceededLimit && (
+        <p className="text-red-500 relative top-5">Você atingiu o limite máximo de 100 unidades!</p>
+      )}
+    </div>
+  );
 }
